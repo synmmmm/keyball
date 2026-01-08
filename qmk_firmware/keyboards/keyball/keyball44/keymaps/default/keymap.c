058be7ca96cc7cd43c8176c7f30fe20c62d7c295
shin.m <synornithe@gmail.com>
2026-01-08 22:35:41 +0900

    fix: trackball config

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -36,28 +36,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                 KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                 KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
     KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                 KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSPC  ,
-                          KC_NO    , KC_LALT  , KC_LGUI  ,MO(1)     , KC_SPC   ,           KC_ENT   , MO(2)    , KC_NO    , KC_NO               , TO(3)
+                          TO(3)    , KC_LALT  , KC_LGUI  ,MO(1)     , KC_SPC   ,           KC_ENT   , MO(2)    , KC_NO    , KC_NO               , KC_NO
   ),
 
   [1] = LAYOUT_universal(
     KC_GRAVE , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                 KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_EQL   ,
     _______  , KC_LNG1  , KC_NO    ,G(KC_PSCR), KC_HOME  , KC_PGUP  ,                                 KC_NO    , KC_NO    , KC_NO    , KC_LBRC  , KC_RBRC  , KC_BSLS  ,
     _______  , KC_LNG2  ,S(KC_CAPS), KC_NO    , KC_END   , KC_PGDN  ,                                 KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
-                          KC_NO    , _______  , _______  , _______  , _______  ,           _______  , KC_NO    , _______  , _______             , _______
+                          _______  , _______  , _______  , _______  , _______  ,           _______  , KC_NO    , _______  , _______             , _______
   ),
 
   [2] = LAYOUT_universal(
     KC_ESC   , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                 KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______  , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , KC_NO    , KC_F12   ,
     _______  , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_DEL   ,
-                          KC_NO    , _______  , _______  , KC_NO    , _______  ,           _______  , _______  , _______  , _______             , _______
+                          _______  , _______  , _______  , KC_NO    , _______  ,           _______  , _______  , _______  , _______             , _______
   ), 
 
   [3] = LAYOUT_universal(
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , KC_NO    , KC_NO    , CPI_D100 , CPI_I100 , ACC_TOG  ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 SCRL_DVI , SCRL_DVD , SSNP_VRT , SSNP_HOR , SSNP_FRE , KC_NO    ,
-    KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , KC_BTN1  , KC_BTN2  , KC_NO    , SCRL_MO  , KC_NO    ,
-                          QK_BOOT  , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,           DPI_800  , DPI_1200 , _______  , _______             , TO(0)
+    KC_TAB   , KC_NO    , KC_NO    , KC_NO    , KBC_RST  , KBC_SAVE ,                                 KC_NO    , KC_NO    , KC_NO    , CPI_D100 , CPI_I100 , ACC_TOG  ,
+    _______  , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 SCRL_DVI , SCRL_DVD , SSNP_VRT , SSNP_HOR , SSNP_FRE , KC_NO    ,
+    _______  , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                 KC_NO    , KC_BTN1  , KC_BTN2  , KC_NO    , SCRL_MO  , KC_NO    ,
+                          TO(0)    , _______  , _______  , KC_NO    , KC_NO    ,           _______  , _______  , _______  , _______             , KC_NO
   ),
 };
 // clang-format on
